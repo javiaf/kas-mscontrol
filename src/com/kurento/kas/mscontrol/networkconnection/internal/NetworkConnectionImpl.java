50d7596dd4c1bbd4026ef1d4ef11863e143759e2
Miguel París Díaz <mparisdiaz@gmail.com>
2011-12-19 10:44:08 +0100

    Format NetworkConnectionImpl.

--- src/com/kurento/kas/mscontrol/networkconnection/internal/NetworkConnectionImpl.java
+++ src/com/kurento/kas/mscontrol/networkconnection/internal/NetworkConnectionImpl.java
@@ -250,8 +250,8 @@ public class NetworkConnectionImpl extends NetworkConnectionBase {
 					addPayloadSpec(videoList, payload + " H263-1998/90000",
 							MediaType.VIDEO, videoPort, h263fp);
 				} else if (VideoCodecType.H264.equals(vp.getVideoCodecType())) {
-					addPayloadSpec(videoList, payload + " H264/90000", MediaType.VIDEO, videoPort,
-							null);
+					addPayloadSpec(videoList, payload + " H264/90000",
+							MediaType.VIDEO, videoPort, null);
 				}
 				payload++;
 			}
