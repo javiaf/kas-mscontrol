5166742ef2e4902f622fcee0ba29e686544e4ecd
Miguel París Díaz <mparisdiaz@gmail.com>
2012-04-13 20:12:29 +0200

    AudioRecorderComponent implements Recorder to be controlled by RecorderController.

--- src/com/kurento/kas/mscontrol/mediacomponent/internal/AudioRecorderComponent.java
+++ src/com/kurento/kas/mscontrol/mediacomponent/internal/AudioRecorderComponent.java
@@ -32,7 +32,8 @@ import com.kurento.kas.media.rx.AudioRx;
 import com.kurento.kas.media.rx.AudioSamples;
 import com.kurento.kas.mscontrol.join.AudioJoinableStreamImpl;
 
-public class AudioRecorderComponent extends MediaComponentBase implements AudioRx {
+public class AudioRecorderComponent extends MediaComponentBase implements
+		Recorder, AudioRx {
 
 	private static final String LOG_TAG = "NDK-audio-rx"; // "AudioRecorder";
 
@@ -44,6 +45,27 @@ public class AudioRecorderComponent extends MediaComponentBase implements AudioR
 	private AudioTrackControl audioTrackControl = null;
 
 	private BlockingQueue<AudioSamples> audioSamplesQueue;
+
+	private final Object controll = new Object();
+
+	private boolean isRecording = false;
+	private long targetPtsNorm;
+
+	public synchronized boolean isRecording() {
+		return isRecording;
+	}
+
+	public synchronized void setRecording(boolean isRecording) {
+		this.isRecording = isRecording;
+	}
+
+	public synchronized long getTargetPtsNorm() {
+		return targetPtsNorm;
+	}
+
+	public synchronized void setTargetPtsNorm(long targetPtsNorm) {
+		this.targetPtsNorm = targetPtsNorm;
+	}
 
 	@Override
 	public synchronized boolean isStarted() {
@@ -93,10 +115,15 @@ public class AudioRecorderComponent extends MediaComponentBase implements AudioR
 		}
 		audioTrackControl = new AudioTrackControl();
 		audioTrackControl.start();
+
+		RecorderControllerComponent.getInstance().addRecorder(this);
+		Log.d(LOG_TAG, "add to controller");
 	}
 
 	@Override
 	public synchronized void stop() {
+		RecorderControllerComponent.getInstance().deleteRecorder(this);
+
 		if (audioTrackControl != null)
 			audioTrackControl.interrupt();
 		if (audioTrack != null) {
@@ -112,13 +139,35 @@ public class AudioRecorderComponent extends MediaComponentBase implements AudioR
 			try {
 				AudioSamples audioSamplesProcessed;
 				for (;;) {
+					if (!isRecording()) {
+						synchronized (controll) {
+							controll.wait();
+						}
+						continue;
+					}
+
 					if (audioSamplesQueue.isEmpty())
 						Log.w(LOG_TAG, "jitter_buffer_underflow: Audio frames queue is empty");
 
+					long targetPtsNorm = getTargetPtsNorm();
+					if (targetPtsNorm != -1) {
+						long ptsNorm = calcPtsNorm(audioSamplesQueue.peek());
+						Log.d(LOG_TAG, "ptsNorm: " + ptsNorm + " targetPts: "
+								+ targetPtsNorm);
+						if ((ptsNorm == -1) || (ptsNorm > targetPtsNorm)) {
+							Log.d(LOG_TAG, "wait");
+							synchronized (controll) {
+								controll.wait();
+							}
+							continue;
+						}
+					}
+
 					audioSamplesProcessed = audioSamplesQueue.take();
-					Log.d(LOG_TAG, "play frame");
+					Log.d(LOG_TAG, "play audio samples");
 					if (audioTrack != null
 							&& (audioTrack.getPlayState() == AudioTrack.PLAYSTATE_PLAYING)) {
+						Log.d(LOG_TAG, "ok");
 						audioTrack.write(
 								audioSamplesProcessed.getDataSamples(), 0,
 								audioSamplesProcessed.getSize());
@@ -128,6 +177,43 @@ public class AudioRecorderComponent extends MediaComponentBase implements AudioR
 				Log.d(LOG_TAG, "AudioTrackControl stopped");
 			}
 		}
+	}
+
+	@Override
+	public long getPtsNorm() {
+		return calcPtsNorm(audioSamplesQueue.peek());
+	}
+
+	@Override
+	public boolean hasMediaPacket() {
+		return !audioSamplesQueue.isEmpty();
+	}
+
+	@Override
+	public void startRecord() {
+		startRecord(-1);
+	}
+
+	@Override
+	public void startRecord(long pts) {
+		setTargetPtsNorm(pts);
+		setRecording(true);
+		synchronized (controll) {
+			controll.notify();
+		}
+	}
+
+	@Override
+	public void stopRecord() {
+		setRecording(false);
+	}
+
+	private long calcPtsNorm(AudioSamples as) {
+		if (as == null)
+			return -1;
+
+		return 1000 * ((as.getPts() - as.getStartTime()) * as.getTimeBaseNum())
+				/ as.getTimeBaseDen();
 	}
 
 }
