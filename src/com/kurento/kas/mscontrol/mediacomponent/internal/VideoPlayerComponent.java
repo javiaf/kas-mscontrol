9006fd423db53ffd25aa7ec497db3a276aeee88a
Raúl Benítez <raulbenitezmejias@gmail.com>
2012-05-22 15:29:07 +0200

    Delete the code that tried implement the change of the orientation

--- src/com/kurento/kas/mscontrol/mediacomponent/internal/VideoPlayerComponent.java
+++ src/com/kurento/kas/mscontrol/mediacomponent/internal/VideoPlayerComponent.java
@@ -245,47 +245,6 @@ public class VideoPlayerComponent extends MediaComponentBase implements
 				+ " x " + parameters.getPreviewSize().height);
 		Log.d(LOG_TAG, "getSupportedPreviewSizes:\n" + cad);
 
-		// int result = 0;
-		// if (VERSION.SDK_INT < 9) {
-		// result = (360 + 90 - screenOrientation) % 360;
-		// // mCamera.setDisplayOrientation(result);
-		//
-		// } else {
-		//
-		// android.hardware.Camera.CameraInfo info = new
-		// android.hardware.Camera.CameraInfo();
-		// android.hardware.Camera.getCameraInfo(0, info);
-		// int rotation = screenOrientation;
-		// int degrees = 0;
-		// switch (rotation) {
-		// case Surface.ROTATION_0:
-		// degrees = 0;
-		// break;
-		// case Surface.ROTATION_90:
-		// degrees = 90;
-		// break;
-		// case Surface.ROTATION_180:
-		// degrees = 180;
-		// break;
-		// case Surface.ROTATION_270:
-		// degrees = 270;
-		// break;
-		// }
-		//
-		// if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
-		// result = (info.orientation + degrees) % 360;
-		// result = (360 - result) % 360; // compensate the mirror
-		// } else { // back-facing
-		// result = (info.orientation - degrees + 360) % 360;
-		// }
-		// Log.d(LOG_TAG, "info.orientation = " + info.orientation
-		// + "; Result-Orientation = " + result);
-		// mCamera.setDisplayOrientation(result);
-		//
-		// }
-		// parameters.setRotation(result);
-		// mCamera.setParameters(parameters);
-
 		try {
 
 			mCamera.setPreviewDisplay(mHolder2);
